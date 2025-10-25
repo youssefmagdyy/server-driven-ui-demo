c1b007f072ae18ecfaf743a73f421bf8f4de1916
youssefmagdyy <youssefmagdy1910@gmail.com>
2025-10-25 18:37:50 +0300

    git init

---
+++ src/main/java/com/youssef/server_ui_demo/ServerUiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.server_ui_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUiDemoApplication.class, args);
+	}
+
+}
